6615c052403d089d281c4ed27befdcde11d46449
Agnerft <59939932+Agnerft@users.noreply.github.com>
2024-01-04 09:30:26 -0300

    Add files via upload

---
+++ python/casterUpdate/updateCaster.bat
@@ -0,0 +1,9 @@
+chcp 65001
+@echo off
+title Baixar Caster
+echo ==================================================================
+echo Programa em java para Automações!
+echo ==================================================================
+echo.
+wget -P caster -N "https://portal.makesystem.com.br/caster/Caster.jar"
+cls
