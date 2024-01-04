086d57542dff9ea671e1fadbca53a47843aa466f
Agnerft <59939932+Agnerft@users.noreply.github.com>
2024-01-04 11:55:45 -0300

    Update updateCaster.bat

--- python/casterUpdate/updateCaster.bat
+++ python/casterUpdate/updateCaster.bat
@@ -2,8 +2,11 @@ chcp 65001
 @echo off
 title Baixar Caster
 echo ==================================================================
-echo Programa em java para Automações!
+echo Programa em java para AutomaÃ§Ãµes!
 echo ==================================================================
 echo.
 wget -P caster -N "https://portal.makesystem.com.br/caster/Caster.jar"
+
+java -jar ./caster/Caster.jar
+
 cls
